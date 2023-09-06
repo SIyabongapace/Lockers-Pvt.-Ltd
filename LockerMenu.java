00a26a7d9245da541790becaf4d973ecb1302985
SIyabongapace <144215271+SIyabongapace@users.noreply.github.com>
2023-09-06 13:17:22 +0200

    Add files via upload

---
+++ LockerMenu.java
@@ -0,0 +1,15 @@
+package main.directory;
+
+public class LockerMenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void menu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
